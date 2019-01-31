2cae22aaf697f2cd893ae6fb4178b2708765820c
Brandon Park <brandonpark206@gmail.com>
2019-01-30 17:43:37 -0800

    fixing bugs for scraping regex data

--- scraper.js
+++ scraper.js
@@ -45,19 +45,30 @@ function getMovie (imdbID) {
             const $rating = $('.title_wrapper .subtext');
             const rating = $rating.text().trim().match(/[^|]*/)[0].trim();
             const runTime = $('.subtext time').text().trim();
-            const $genres = $('.subtext a');
-            const genres = $genres.text().match(/^(.*?)\d/)[1].replace(/([a-z])([A-Z])/g, '$1 $2');
-            const releaseDate = $('a[title="See more release dates"]').text().trim();
+            const genres = [];
+            $('.subtext a').each(function(i, element){
+                const genre = $(element).text().trim();
+                genres.push(genre);
+                });
+            const releaseDate = genres.pop();
             const imdbRating = $('span[itemProp="ratingValue"]').text();
             const poster = $('div.poster a img').attr('src');
             const summary = $('div.summary_text').text().trim();
-            const director = $('.credit_summary_item a').first().text();
-            const writer = $('.credit_summary_item a').slice(1,2).text();
-            const stars = [];
-            const star1 = $('.credit_summary_item a').slice(2).eq(0).text();
-            const star2 = $('.credit_summary_item a').slice(2).eq(1).text();
-            const star3 = $('.credit_summary_item a').slice(2).eq(2).text();
-            stars.push(star1, star2, star3);
+            const directors = [];
+            $('.plot_summary .credit_summary_item').eq(0).each(function(i, element){
+                const director = $(element).text().trim().match(/(?<=\n).*/)[0];
+                // const director = $(element).text().trim().match(/[^\:]*/);
+                console.log(director);
+                directors.push(director);
+            });
+
+            // const $writers = $('.plot_summary .credit_summary_item').eq(1).text().trim().match(/.+,(.+)/)[0];
+            // const writers = $writers.match(/[^|]*/)[0].trim();
+            // const stars = [];
+            // const star1 = $('.credit_summary_item a').slice(2).eq(0).text();
+            // const star2 = $('.credit_summary_item a').slice(2).eq(1).text();
+            // const star3 = $('.credit_summary_item a').slice(2).eq(2).text();
+            // stars.push(star1, star2, star3);
 
             return {
                 imdbID,
@@ -69,10 +80,9 @@ function getMovie (imdbID) {
                 imdbRating,
                 poster,
                 summary,
-                director,
-                writer,
-                stars,
-
+                directors,
+                // writers,
+                // stars,
             }
         });
 }
